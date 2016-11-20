9aa046a49c72a5519f8e3e862e0c1252e623e2a1
Martin Holm Jensen <martin.holm@leoilab.com>
2016-11-20 23:14:17 +0100

    README updated tooling cleaned up

--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,7 @@ let development = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-      },
-      // the url-loader uses DataUrls.
-      // the file-loader emits files.
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+      }
     ]
   },
   plugins: [
