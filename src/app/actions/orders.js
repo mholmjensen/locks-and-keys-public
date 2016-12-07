37365667dcd309d6c90183158dbc66d0b0f16faf
Martin Holm Jensen <martin.holm@leoilab.com>
2016-12-07 22:28:08 +0100

    Refactored into rfgrid client and update values in backend action

--- src/app/actions/orders.js
+++ src/app/actions/orders.js
@@ -1,7 +1,8 @@
 /* @flow weak */
 
-import { SET_ORDERS, SET_SELECTED_ORDER } from '../constants'
+import { UPDATE_ORDER_VALUES, SET_ORDERS, SET_SELECTED_ORDER } from '../constants'
 import { jsonParse, statusCheck, requestFailed } from './async.utils'
+import rfgrid from './rfgrid'
 
 export function setOrders (orders) {
   return {
@@ -19,61 +20,46 @@ export function setSelectedOrder (order) {
   }
 }
 
-// Async actions
-
-let ordersUrl = 'backend/rfgrid/octopussy/plumbingorders'
-let authUrl = 'backend/rfgrid/octopussy/token'
-let authChallenge = {
-  method: 'POST',
-  body: 'grant_type=password&username=rfgriduser&password=rfgriduser',
-  headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }),
-  mode: 'no-cors'
-}
-
-let ACCESSTOKEN = ''
-let authedPipe = (additionalRequestOpts = {}) => {
-  if (ACCESSTOKEN) {
-    let authHeaders = new Headers()
-    authHeaders.append('Authorization', 'Bearer ' + ACCESSTOKEN)
-    let authedOpts = { method: 'GET', headers: authHeaders, mode: 'cors' } // CORS mode necessary for Authorization header to be acceptable to fetch
-    return Object.assign({}, authedOpts, additionalRequestOpts)
-  } else {
-    throw Error('ACCESSTOKEN not set, cannot make authedPipe')
+export function updateOrderValues (uuid, values) {
+  return {
+    type: UPDATE_ORDER_VALUES,
+    payload: { uuid, values }
   }
 }
 
-function rfgridAuthentication () {
-  return fetch(authUrl, authChallenge)
-    .then(statusCheck)
-    .then(jsonParse)
-    .then((r) => { return r.access_token })
-    .catch(requestFailed)
-}
-
+// Async actions
 export function rfgridClientLogin () {
   return dispatch => {
-    return rfgridAuthentication()
-    .then((grantedAccessToken) => {
-      ACCESSTOKEN = grantedAccessToken
-      return true
-    })
-    .catch(requestFailed)
+    return rfgrid.login()
   }
 }
 
-export function getOrdersAsync () {
+let ordersUrl = 'backend/rfgrid/octopussy/plumbingorders'
+export function getOrdersAsync (limit = 50) {
   return dispatch => {
-    // TODO pagination
-    return fetch(ordersUrl + '?ql=select * order by human_readable_id&limit=30', authedPipe())
+    return rfgrid.authedRequest('plumbingorders?ql=select * order by human_readable_id&limit=' + 50)
     .then(statusCheck)
     .then(jsonParse)
     .then(data => {
       dispatch(setOrders(data.entities))
-      return data
-    })
-    .then(data => {
       if (data.entities.length > 0) {
         dispatch(setSelectedOrder(data.entities[0]))
+      }
+      return data
+    })
+    .catch(requestFailed)
+  }
+}
+
+export function saveOrderData (uuid, values) {
+  return dispatch => {
+    let updateRequest = { method: 'PUT', body: JSON.stringify(values), mode: 'cors' }
+    return rfgrid.authedRequest('plumbingorders/' + uuid, updateRequest)
+    .then(statusCheck)
+    .then(jsonParse)
+    .then(result => {
+      if (result.entities.length > 0) {
+        dispatch(updateOrderValues(uuid, result.entities[0]))
       }
     })
     .catch(requestFailed)
--- src/app/reducers/orders.js
+++ src/app/reducers/orders.js
@@ -1,6 +1,6 @@
 /* @flow */
-
-import { SET_ORDERS, SET_SELECTED_ORDER } from '../constants'
+import update from 'react-addons-update'
+import { UPDATE_ORDER_VALUES, SET_ORDERS, SET_SELECTED_ORDER } from '../constants'
 
 let ordersInitialState = {
   entries: [],
@@ -9,6 +9,20 @@ let ordersInitialState = {
 
 export default function orders (state: Object = ordersInitialState, action: Object) {
   switch (action.type) {
+    case UPDATE_ORDER_VALUES:
+      let entriesUpdated = state.entries.map((v) => Object.assign({}, v) ) // TODO instead of deepcopy, update entries using react update immutability helpers
+      let orderAt = state.entries.findIndex((order) => order.uuid === action.payload.uuid )
+      if (orderAt > -1 && action.payload.values) {
+          entriesUpdated[orderAt] = Object.assign({}, entriesUpdated[orderAt], action.payload.values)
+      } else {
+        console.log('Error, state unmodified. Trying to', UPDATE_ORDER_VALUES, orderAt, action.payload.values)
+      }
+
+      return {
+        ...state,
+        entries: entriesUpdated
+      }
+
     case SET_ORDERS:
       return {
         ...state,
