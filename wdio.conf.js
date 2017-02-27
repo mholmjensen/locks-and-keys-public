08768b377d31bd54522f63634f6e65475b943d4c
Martin Holm Jensen <martin.holm@leoilab.com>
2017-02-28 00:01:51 +0100

    protocol specified

--- wdio.conf.js
+++ wdio.conf.js
@@ -72,7 +72,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'localhost',
+    baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
